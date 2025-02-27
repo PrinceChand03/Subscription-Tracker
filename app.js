40da4f1e2640e4c4359abec5e5c159c9988a36b4
Jonathan-Ultini <jonathanultini@gmail.com>
2025-02-28 00:58:56 +0100

    add error handlers in middleware for debugging

--- app.js
+++ app.js
@@ -7,12 +7,25 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+// handle json data in API calls
+app.use(express.json());
+
+// process the form data sent via html forms in a simple format
+app.use(express.urlencoded({ extended: false }));
+
+// reads coockies from incoming request to store user data
+app.use(cookieParser());
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the subscription tracker API!');
