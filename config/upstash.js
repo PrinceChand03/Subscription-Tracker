ed33c524c83df0d9b307f661ff616ab04744edf7
Jonathan-Ultini <jonathanultini@gmail.com>
2025-03-11 16:59:17 +0100

    feat: add QStash configuration and WorkflowClient initialization

---
+++ config/upstash.js
@@ -0,0 +1,8 @@
+import { Client as WorkflowClient } from '@upstash/workflow';
+
+import { QSTASH_TOKEN, QSTASH_URL } from './env';
+
+export const WorkflowClient = new WorkflowClient({
+  token: QSTASH_TOKEN,
+  url: QSTASH_URL,
+});
