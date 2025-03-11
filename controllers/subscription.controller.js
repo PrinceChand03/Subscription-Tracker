808685408ac620131273c86f1601c8bcc45a1fad
Jonathan-Ultini <jonathanultini@gmail.com>
2025-03-11 18:40:11 +0100

    feat: integrate workflow client for triggering subscription workflows

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,5 @@
 import Subscription from '../models/subscription.model.js';
+import { workflowClient } from '../config/upstash.js';
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -7,6 +8,10 @@ export const createSubscription = async (req, res, next) => {
       ...req.body,
       user: req.user._id
     });
+
+    await workflowClient.trigger({
+      url: `${SERVER_URL}`
+    })
 
     res.status(201).json({ success: true, data: subscription });
   } catch (e) {
