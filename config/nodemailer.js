3bf1b1c2ff614686f14c8a22b4e2f83f5e246e0f
Jonathan-Ultini <jonathanultini@gmail.com>
2025-03-12 22:17:29 +0100

    feat: implement sendReminderEmail function for subscription renewal notifications

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,6 +1,8 @@
 import nodemailer from 'nodemailer';
 import { EMAIL, EMAIL_PASSWORD } from './env.js';
 
+export const accountEmail = EMAIL;
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
