2aca39ef06df8758f64f9fccec4d0a0bb96c507e
Jonathan-Ultini <jonathanultini@gmail.com>
2025-03-15 00:03:25 +0100

    feat: add cancelSubscription function with authorization and error handling

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -36,3 +36,28 @@ export const getUserSubscriptions = async (req, res, next) => {
     next(e);
   }
 }
+
+
+export const cancelSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.findById(req.params.id);
+
+    if (!subscription) {
+      const error = new Error('Subscription not found');
+      error.status = 404;
+      throw error;
+    }
+
+    if (subscription.user.toString() !== req.user._id.toString()) {
+      const error = new Error('You are not authorized to cancel this subscription');
+      error.status = 403;
+      throw error;
+    }
+
+    await subscription.remove();
+
+    res.status(200).json({ success: true, message: 'Subscription cancelled successfully' });
+  } catch (e) {
+    next(e);
+  }
+};
